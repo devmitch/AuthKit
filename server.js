5c71a447ddd0312b243a4e397f2baf503460ff38
Mitchell Henry <mitchell.henry334@gmail.com>
2020-11-10 21:43:40 +1100

    sqlite3 stuff

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3').verbose();
 
 let app = express();
 
 app.use(bodyParser.json());
 
+app.post('/create_identity', (req, res, next) => {
+    // email, password
+    let db = new sqlite3.Database('./id.db', (err) => {
+        if (err) {
+            console.log(err.message);
+        }
+        console.log("connected to sqlite db!");
+    });
+
+    db.close()
+});
+
 app.listen(8080, () => {
     console.log("Server started!");
 });
+
+//USE UUID FOR THE TOKEN, store info (user id, claims, etc) in the redis document
